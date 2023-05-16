c391142464a2dfea26dc20fb2200de3969e9fcde
Daniel Sun <dansun314@gmail.com>
2023-05-15 23:38:29 -0400

    Added optional useroption

--- src/commands/getFine.ts
+++ src/commands/getFine.ts
@@ -2,25 +2,46 @@ import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
 import { Command } from "../interfaces/Command";
 import { updateMemberData } from "../modules/updateMemberData";
 import { getMemberData } from "../modules/getMemberData";
+import { MemberInt } from "../db/models/MemberModel";
 
 export const getFine: Command = {
     data: new SlashCommandBuilder()
         .setName("getfine")
-        .setDescription("Check your fine"),
+        .setDescription("Check your fine")
+        .addUserOption((option) =>
+            option
+                .setName("member")
+                .setDescription("Member to check fine of")
+                .setRequired(false)
+        ),
     run: async (interaction) => {
         await interaction.deferReply();
-        const { user } = interaction;
-        const targetMember = await getMemberData(user.id);
+        const memberOption = interaction.options.get("member");
+        let targetMember: MemberInt | null = null;
+        if (memberOption) {
+            const member = memberOption;
+            targetMember = await getMemberData(member.value as string);
+        } else {
+            const { user } = interaction;
+            targetMember = await getMemberData(user.id);
+        }
 
         if (targetMember !== null) {
         const fineEmbed = new EmbedBuilder();
+
+        let finesAndReasons = "";
+        targetMember.fines.forEach((fine, index) => {
+            finesAndReasons += `${index + 1}: ${fine.amount} - ${fine.reason}\n`;
+        });
+
         fineEmbed
-            .setTitle(`Fine for @${ user.tag }`)
+            .setTitle(`Fines`)
+            .setDescription(`for ${ interaction.user }`)
             .addFields(
-                { name: "Current Fine", value: targetMember.fine.toString() });
+                { name: "Fines", value: finesAndReasons });
         await interaction.editReply({ embeds: [fineEmbed] });
         } else {
-            await interaction.editReply("You are not registered!");
+            await interaction.editReply("User is not a registered member!");
         }
     },
 }
