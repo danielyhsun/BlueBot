094d3126cf322b82a7c3cc30de23c5b0a658a8d5
Daniel Sun <dansun314@gmail.com>
2023-05-16 09:26:16 -0400

    consolidated previous commands into subcommands

---
+++ src/commands/events.ts
@@ -0,0 +1,120 @@
+import {
+  SlashCommandBuilder,
+  GuildScheduledEventEntityType,
+  GuildScheduledEventPrivacyLevel,
+  TextInputBuilder,
+  TextInputStyle,
+  ModalBuilder,
+  ActionRowBuilder,
+  CommandInteractionOptionResolver,
+  CacheType,
+  Role,
+} from "discord.js";
+import { type Command } from "../interfaces/Command";
+import * as chrono from "chrono-node";
+import { addEventData } from "../modules/addEventData";
+import { refreshScheduler } from "../modules/refreshScheduler";
+
+// Add Event to Calendar and Google Sheet so then bros can get marked down for attendance
+export const events: Command = {
+  data: new SlashCommandBuilder()
+    .setName("events")
+    .setDescription("Commands for events")
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("create")
+        .setDescription("Creates new event")
+        .addRoleOption((role) =>
+          role
+            .setName("group")
+            .setDescription("Assign attendance to event for a specific role")
+            .setRequired(false)
+        )
+    ),
+  run: async (interaction) => {
+    const options = interaction.options as CommandInteractionOptionResolver<CacheType>;
+    const command = options.getSubcommand();
+
+    if (command == "create") {
+      const eventModal = new ModalBuilder()
+        .setCustomId("CreateEventModal")
+        .setTitle("Create Event");
+
+      const eventNameInput = new TextInputBuilder()
+        .setCustomId("eventNameInput")
+        .setLabel("Event Name")
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true);
+      const dateTimeInput = new TextInputBuilder()
+        .setCustomId("dateTimeInput")
+        .setLabel("Event Date and Time")
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true);
+      const timeLengthInput = new TextInputBuilder()
+        .setCustomId("timeLengthInput")
+        .setLabel("Event Length (in hours)")
+        .setStyle(TextInputStyle.Short)
+        .setPlaceholder("Default - 1 hour")
+        .setRequired(false);
+      const locationInput = new TextInputBuilder()
+        .setCustomId("locationInput")
+        .setLabel("Event Location")
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true);
+
+      const firstActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(eventNameInput);
+      const secondActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(dateTimeInput);
+      const thirdActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(timeLengthInput);
+      const fourthActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(locationInput);
+
+      eventModal.addComponents(firstActionRow, secondActionRow, thirdActionRow, fourthActionRow);
+
+      // Display the modal
+      await interaction.showModal(eventModal);
+
+      const role = options.getRole("group") as Role;
+      let membersWithRole = [];
+      if (role) {
+        membersWithRole = role.members.map(member => member.id);
+        console.log(membersWithRole);
+      }
+
+      // Wait for modal submission
+      interaction
+        .awaitModalSubmit({ time: 60_000 })
+        .then(async (interaction) => {
+          const { guild } = interaction;
+          const eventName = interaction.fields.getTextInputValue("eventNameInput");
+          const eventDate = interaction.fields.getTextInputValue("dateTimeInput");
+          const eventLength = interaction.fields.getTextInputValue("timeLengthInput");
+          const lengthValue = parseInt(eventLength) || 1;
+          const location = interaction.fields.getTextInputValue("locationInput");
+
+          // Create new scheduled event from modal submission input values
+          guild?.scheduledEvents.create({
+            name: eventName,
+            scheduledStartTime: chrono.parseDate(eventDate),
+            scheduledEndTime: chrono
+              .parseDate(eventDate)
+              .setHours(chrono.parseDate(eventDate).getHours() + lengthValue),
+            privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
+            entityType: GuildScheduledEventEntityType.External,
+            entityMetadata: { location },
+          });
+
+          // Add event data to db after the guild scheduled event is created
+          await addEventData(eventName, chrono.parseDate(eventDate));
+          // Refresh scheduler after event data is added to db
+          await refreshScheduler(interaction.client);
+
+          interaction.reply({
+            content: `Event "${eventName}" at ${chrono.parseDate(eventDate)} created!`,
+            ephemeral: true,
+          });
+        })
+        .catch((err) => {
+          console.log("No modal submit interaction collected: " + err);
+        });
+    }
+  },
+};
---
+++ src/commands/fines.ts
@@ -0,0 +1,104 @@
+import {
+  EmbedBuilder,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+  CommandInteraction,
+  CommandInteractionOptionResolver,
+  CacheType,
+} from "discord.js";
+import { type Command } from "../interfaces/Command";
+import { getMemberData } from "../modules/getMemberData";
+import { updateMemberData } from "../modules/updateMemberData";
+import { MemberInt } from "../db/models/MemberModel";
+
+export const fines: Command = {
+  data: new SlashCommandBuilder()
+    .setName("fines")
+    .setDescription("Commands for fines")
+    // "add" subcommand
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("add")
+        .setDescription("Add a fine to a member")
+        .addUserOption((option) =>
+          option.setName("member").setDescription("Member to add fine to").setRequired(true)
+        )
+        .addNumberOption((option) =>
+          option.setName("amount").setDescription("Amount to add").setRequired(true)
+        )
+        .addStringOption((option) =>
+          option.setName("reason").setDescription("Reason for fine").setRequired(true)
+        )
+    )
+    // "list" subcommand
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("list")
+        .setDescription("List fines for a member")
+        .addUserOption((option) =>
+          option.setName("member").setDescription("Member to check fine of").setRequired(false)
+        )
+    )
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  run: async (interaction) => {
+    await interaction.deferReply();
+    const options = interaction.options as CommandInteractionOptionResolver<CacheType>;
+    const command = options.getSubcommand();
+
+    if (command == "add") {
+      // const { user } = interaction;
+      const memberOption = options.get("member");
+      const targetMember = await getMemberData(memberOption?.value as string);
+      if (targetMember !== null) {
+        const amtOption = interaction.options.get("amount");
+        const amt = amtOption?.value as number;
+        const reasonOption = interaction.options.get("reason");
+        const reason = reasonOption?.value as string;
+        await updateMemberData(targetMember, amt, reason);
+        const fineEmbed = new EmbedBuilder();
+        fineEmbed.setTitle("Fine Updated").addFields(
+          // { name: "Amount to Add", value: amt.toString },
+          { name: "Member", value: `${memberOption?.user}` },
+          { name: "Reason", value: reason },
+          { name: "Amount Added", value: amt.toString(), inline: true },
+          { name: "Updated Fine", value: targetMember.fineTotal.toString(), inline: true }
+        );
+        await interaction.editReply({ embeds: [fineEmbed] });
+      } else {
+        await interaction.editReply("User is not a registered member yet!");
+      }
+    } else if (command == "list") {
+      const memberOption = interaction.options.get("member");
+      let targetMember: MemberInt | null;
+      if (memberOption) {
+        const member = memberOption;
+        targetMember = await getMemberData(member.value as string);
+      } else {
+        const { user } = interaction;
+        targetMember = await getMemberData(user.id);
+      }
+
+      if (targetMember !== null) {
+        const fineEmbed = new EmbedBuilder();
+
+        let finesAndReasons = "";
+        targetMember.fines.forEach((fine, index) => {
+          finesAndReasons += `**${index + 1}.** \`${fine.amount} - ${fine.reason}\`\n`;
+        });
+
+        const fineTotal = targetMember.fineTotal;
+
+        fineEmbed
+          .setTitle("Fines")
+          .setDescription(`for ${interaction.user}`)
+          .addFields(
+            { name: "Total", value: fineTotal.toString() },
+            { name: "Fines", value: finesAndReasons }
+          );
+        await interaction.editReply({ embeds: [fineEmbed] });
+      } else {
+        await interaction.editReply("User is not a registered member!");
+      }
+    }
+  },
+};
