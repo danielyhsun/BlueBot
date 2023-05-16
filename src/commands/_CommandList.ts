3bf40947917896bd75eae8ed851985fadaad1ad7
Daniel Sun <dansun314@gmail.com>
2023-05-16 09:25:32 -0400

    - addfine, getfine, addevent, + fine, event

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,8 +1,10 @@
 import { type Command } from "../interfaces/Command";
 import { addEvent } from "./addEvent";
 import { addFine } from "./addFine";
+import { fines } from "./fines";
 import { getFine } from "./getFine";
 import { register } from "./register";
 import { stats } from "./stats";
+import { events } from "./events";
 
-export const CommandList: Command[] = [getFine, addFine, addEvent, stats, register];
+export const CommandList: Command[] = [fines, events, stats, register];
