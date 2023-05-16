f68108f0e2409aaa50c7b9394b75b8b38b0852f9
Daniel Sun <dansun314@gmail.com>
2023-05-15 23:40:16 -0400

    Updated to fit changed model

--- src/modules/updateMemberData.ts
+++ src/modules/updateMemberData.ts
@@ -1,7 +1,8 @@
 import { MemberInt } from "../db/models/MemberModel"
 
-export const updateMemberData = async (Member: MemberInt, fineChange: number) => {
-    Member.fine = Member.fine + fineChange;
+export const updateMemberData = async (Member: MemberInt, fineChange: number, fineReason: string) => {
+    Member.fines.push({ amount: fineChange, reason: fineReason });
+    Member.fineTotal += fineChange;
     await Member.save();
     return Member;
 }
